86055df8bf64980a3f027f5a29cfedc34e8b4310
Verbena Macbook Air <vassidiqi@gmail.com>
2024-12-26 21:43:44 +0700

    attempt to use sib_api_v3_sdk

--- shared/go.mod
+++ shared/go.mod
@@ -2,13 +2,12 @@ module github.com/assidiqi598/umrah-erp/shared
 
 go 1.23.4
 
-require github.com/getbrevo/brevo-go v1.1.2
+require github.com/sendinblue/APIv3-go-library/v2 v2.1.2
 
 require (
 	github.com/golang/snappy v0.0.4 // indirect
 	github.com/klauspost/compress v1.13.6 // indirect
 	github.com/montanaflynn/stats v0.7.1 // indirect
-	github.com/sendinblue/APIv3-go-library/v2 v2.1.2 // indirect
 	github.com/xdg-go/pbkdf2 v1.0.0 // indirect
 	github.com/xdg-go/scram v1.1.2 // indirect
 	github.com/xdg-go/stringprep v1.0.4 // indirect
@@ -16,8 +15,6 @@ require (
 	golang.org/x/crypto v0.26.0 // indirect
 	golang.org/x/sync v0.8.0 // indirect
 	golang.org/x/text v0.17.0 // indirect
-	gopkg.in/alexcesaro/quotedprintable.v3 v3.0.0-20150716171945-2caba252f4dc // indirect
-	gopkg.in/gomail.v2 v2.0.0-20160411212932-81ebce5c23df // indirect
 )
 
 require (
--- shared/utils/send_email.go
+++ shared/utils/send_email.go
@@ -1,17 +1,37 @@
 package utils
 
 import (
-	"gopkg.in/gomail.v2"
+	"context"
+	"fmt"
+
+	sib_api_v3_sdk "github.com/sendinblue/APIv3-go-library/v2/lib"
 )
 
-func SendEmail(smtpKey string, smtpFrom string, senderEmail string, senderName string, recipientEmail string, recipientName string, subject string, textContent string, html string) (string, error) {
+func SendEmail(apiKey string, senderEmail string, senderName string, recipientEmail string, recipientName string, subject string, textContent string, html string) (string, error) {
+	var ctx context.Context
 
-	msg := gomail.NewMessage()
-	msg.SetHeader("From", senderName+" <"+senderEmail+">")
-	msg.SetHeader("To", recipientName+" <"+recipientEmail+">")
-	msg.SetHeader("Subject", subject)
-	msg.SetBody("text/html", html)
+	cfg := sib_api_v3_sdk.NewConfiguration()
 
-	// n := gomail.NewDialer()
-	return "", nil
+	//Configure API key authorization: api-key
+	cfg.AddDefaultHeader("api-key", apiKey)
+
+	sib := sib_api_v3_sdk.NewAPIClient(cfg)
+	body := sib_api_v3_sdk.SendSmtpEmail{
+		HtmlContent: html,
+		Subject:     subject,
+		Sender: &sib_api_v3_sdk.SendSmtpEmailSender{
+			Name:  senderName,
+			Email: senderEmail,
+		},
+		To: []sib_api_v3_sdk.SendSmtpEmailTo{{Name: recipientName, Email: recipientEmail}},
+	}
+	obj, resp, err := sib.TransactionalEmailsApi.SendTransacEmail(ctx, body)
+	if err != nil {
+		fmt.Println(obj)
+		fmt.Println(resp)
+		fmt.Println("Error in TransactionalEmailsApi->SendTransacEmail ", err.Error())
+		return "", err
+	}
+	fmt.Println("SendTransacEmail, response:", resp, "SendTransacEmail object", obj)
+	return obj.MessageId, nil
 }

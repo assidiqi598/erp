7cd3fcb2653a7285cdb270fb7aeff358f18dbd6e
Verbena Macbook Air <vassidiqi@gmail.com>
2024-12-29 17:42:38 +0700

    simplify test command using makefile; update GenerateSecureRandomNumber func; change volumes in mongodb to use path instead

--- Makefile
+++ Makefile
@@ -1,3 +1,5 @@
+.PHONY: setup-grpc compose-up test-auth
+
 PROTO_AUTH_SRC_DIR=services/auth/proto
 PROTO_AUTH_OUT_DIR=services/auth/proto
 PROTO_FLUTTER_OUT_DIR=flutter/lib/proto
@@ -13,3 +15,21 @@ setup-grpc:
 		--proto_path=$(PROTO_AUTH_SRC_DIR) \
 		--dart_out=grpc:$(PROTO_FLUTTER_OUT_DIR) \
 		$(PROTO_AUTH_SRC_DIR)/auth.proto
+
+compose_up:
+	@if ! docker ps | grep -q "auth"; then \
+		echo "Containers not running, starting them..."; \
+		docker compose up -d; \
+	else \
+		echo "Containers are already running."; \
+	fi
+
+check_health: compose_up
+	@until grpcurl -plaintext localhost:50051 list > /dev/null 2>&1; do \
+		echo "Waiting for gRPC server..."; \
+		sleep 2; \
+	done
+	@echo "gRPC server is ready."
+
+test_auth: check_health
+	cd services/auth && go test ./test/... -v
--- shared/utils/secure_random_number.go
+++ shared/utils/secure_random_number.go
@@ -10,13 +10,16 @@ func GenerateSecureRandomNumber(digits int) int {
 		return 0
 	}
 
-	// Calculate the range
-	max := new(big.Int)
-	max.Exp(big.NewInt(10), big.NewInt(int64(digits)), nil).Sub(max, big.NewInt(1))
-	min := new(big.Int)
-	min.Exp(big.NewInt(10), big.NewInt(int64(digits-1)), nil)
+	// Calculate the minimum and maximum values based on the number of digits
+	min := new(big.Int).Exp(big.NewInt(10), big.NewInt(int64(digits-1)), nil) // 10^(digits-1)
+	max := new(big.Int).Exp(big.NewInt(10), big.NewInt(int64(digits)), nil)   // 10^digits
 
-	// Generate a random number in the range
-	num, _ := rand.Int(rand.Reader, new(big.Int).Sub(max, min).Add(max, big.NewInt(1)))
-	return int(num.Add(num, min).Int64())
+	// Generate a random number in the range [min, max)
+	num, _ := rand.Int(rand.Reader, new(big.Int).Sub(max, min)) // max - min is the range
+
+	// Add min to ensure the number is in the correct range
+	num.Add(num, min)
+
+	// Convert to int (assuming the number fits in an int)
+	return int(num.Int64())
 }
